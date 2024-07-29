99f30395f4876d6e3839d3a6930bdbafb901acc5
J. Degand <70610011+jdegand@users.noreply.github.com>
2024-07-29 20:36:58 +0000

    fix: remove unused import & remove duplicated code in ticket spec

--- src/app/list/ticket.store.spec.ts
+++ src/app/list/ticket.store.spec.ts
@@ -1,6 +1,6 @@
 import { render } from "@testing-library/angular";
 import { createMockWithValues } from "@testing-library/angular/jest-utils";
-import { lastValueFrom, of, take, takeLast, throwError } from "rxjs";
+import { lastValueFrom, of, take, throwError } from "rxjs";
 import { APP_ROUTES } from "../app.route";
 import { BackendService } from "../backend.service";
 import { ListComponent } from "./list.component";
@@ -148,89 +148,3 @@ const setup = async () => {
 
   return { fixture, store, mockBackendService };
 };
-
-/*
-import { BackendService } from "../backend.service";
-import { TicketStore } from "./ticket.store";
-import { TestBed } from "@angular/core/testing";
-import { provideComponentStore } from "@ngrx/component-store";
-
-const USERS = [
-  { id: 1, name: 'titi' },
-  { id: 2, name: 'george' },
-];
-const TICKETS = [
-  {
-    id: 0,
-    description: 'Install a monitor arm',
-    assigneeId: 1,
-    completed: false,
-  },
-  {
-    id: 1,
-    description: 'Coucou',
-    assigneeId: 1,
-    completed: false,
-  },
-];
-
-// maybe better to use TestBed
-
-describe('TicketStore', () => {
-
-  let ticketStore: TicketStore;
-  let backendServiceSpy: any;
-
-  beforeEach(() => {
-    const backendServiceSpyObj = {
-      newTicket: jest.fn(),
-      update: jest.fn(),
-    }
-
-    TestBed.configureTestingModule({
-      providers: [
-        provideComponentStore(TicketStore),
-        { provide: BackendService, useValue: backendServiceSpyObj },
-      ],
-    });
-
-    ticketStore = TestBed.inject(TicketStore);
-
-    backendServiceSpy = TestBed.inject(BackendService) as unknown as typeof backendServiceSpyObj;
-
-    ticketStore.setState({ users: USERS, tickets: TICKETS, loading: false, error: false, search: '' });
-  });
-
-  it('should be created', () => {
-    expect(ticketStore).toBeTruthy();
-  });
-
-  describe('When init', () => {
-    it('Then calls backend.users', async () => {
-      // 
-    });
-
-    it('Then calls backend.tickets', async () => {
-      //
-    });
-
-    describe('Given all api returns success response', () => {
-      it('Then tickets and users should be merged ', async () => {
-        //
-      });
-    });
-
-    describe('Given users api returns failure response', () => {
-      it('Then tickets should not have any assignee', () => {
-        //
-      });
-    });
-
-    describe('When adding a new ticket with success', () => {
-      it('Then ticket is added to the list', async () => {
-        //
-      });
-    });
-  });
-});
-*/
